6120d48554e326c03765a7092adbb7f04974d2c6
tw <www.1215168728@qq.com>
2023-11-10 04:06:58 +0800

    加入负载均衡器

--- ali-user-demo/src/main/java/com/example/aliuserdemo/AliUserDemoApplication.java
+++ ali-user-demo/src/main/java/com/example/aliuserdemo/AliUserDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
-
+@ComponentScan("com.example.aliuserdemo.*")
 public class AliUserDemoApplication {
 
     public static void main(String[] args) {
